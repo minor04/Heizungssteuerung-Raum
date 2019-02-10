8d7a5a3f5788e0d4e15668d061fae44dcfd6dc91
minor04 <46103212+minor04@users.noreply.github.com>
2019-02-10 13:18:58 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -139,7 +139,8 @@ class HeizungssteuerungRaum extends IPSModule
 
 		else{
         	SetValue($this->GetIDForIdent("SW_Ra"), ($sw_regler + $sw_ra_anp));					// Raumsollwert für Anzeige
-		SetValue($this->ReadPropertyInteger("SW_An"), ($sw_regler + $sw_ra_anp));
+		//SetValue($this->ReadPropertyInteger("SW_An"), ($sw_regler + $sw_ra_anp));
+		ZW_ThermostatSetPointSet($this->ReadPropertyInteger("SW_An"), 1, ($sw_regler + $sw_ra_anp));
 			
 	    		if($programm <= 3 and (($sw_regler + $sw_ra_anp + $Histerese_aus) <= $rt)){
 		    		SetValue($this->GetIDForIdent("Ventil"), false);
